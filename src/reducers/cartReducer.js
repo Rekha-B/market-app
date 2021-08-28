742f94dd91ccba67905e6faed88eb0b76e372591
Rekha <rekha.b@olaelectric.com>
2021-08-28 13:14:52 +0530

    Added boilerplate code for react and redux

---
+++ src/reducers/cartReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  cartTotal: 0
+};
+const cartReducer = (state = initialState, action) => {
+switch(action.type) {
+  case ADD_ONE:
+    return {
+      cartTotal: state.cartTotal + action.value
+    };
+  default:
+    return state;
+  }
+}
+export default cartReducer;
