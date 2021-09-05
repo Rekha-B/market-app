ce2fd15ad24c6add201f8a875ed10b6b76c00deb
Rekha <rekha.b@olaelectric.com>
2021-09-05 18:17:34 +0530

    git ignore case

---
+++ src/components/FilterPanel/filterPanel.js
@@ -0,0 +1,32 @@
+import { useSelector } from 'react-redux';
+import FilterSection from '../FilterSection/filterSection';
+
+const FilterPanel = () => {
+  const { price, brands, tags } = useSelector((state) => state.productsReducer);
+  return (
+    <section className="filter-panel">
+      {price && price.length > 0 && <FilterSection
+        title="Sorting"
+        data={price}
+        type="radio"
+        id="price"
+      /> }
+      {brands && brands.length > 0 && <FilterSection
+        title="Brands"
+        data={brands}
+        type="checkbox"
+        placeholder="Search brand"
+        id="brands"
+      /> }
+      {tags && tags.length > 0 && <FilterSection
+        title="Tags"
+        data={tags}
+        type="checkbox"
+        placeholder="Search tag"
+        id="tags"
+      /> } 
+    </section>
+  );
+};
+
+export default FilterPanel;
